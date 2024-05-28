c2518bbcad67305f4fa4bbc66387814a9b563849
shimiharu1012 <shimizu1012@ezweb.ne.jp>
2024-05-28 18:44:28 +0900

    プロトタイプが完成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,56 @@
 import {NextPage} from "next";
+import { useEffect, useState } from "react";
+
 
 const IndexPage: NextPage=()=>{
-    return <div>猫画像予定地</div>;
+    const [ImageUrl,setImageUrl]=useState("");
+    const [loading,setLoading]=useState(true);
+
+    useEffect(()=>{
+        fetchImage().then((newImage)=>{
+            setImageUrl(newImage.url);
+            setLoading(false);
+        });
+    },[]);
+
+    // useEffect(()=>{
+    //     console.log(loading)
+    // },[loading])
+    //　ボタンをクリックしたときに画像を読み込む処理
+    // const handleClick=async()=>{
+    //     setLoading(true);
+    //     const newImage=await fetchImage();
+    //     setImageUrl(newImage.url)
+    //     setLoading(false);
+    // }
+    
+    const handleClick=()=>{
+        setLoading(true);
+        fetchImage().then((newImage)=>{
+            setImageUrl(newImage.url)
+            setLoading(false)
+        })
+    }
+
+    return(
+        <div className="">
+            <button onClick={handleClick}>他のニャンコも見る</button>
+            <div>{loading ? <p>LOADING</p> : <img src={ImageUrl}/>}</div>
+        </div>
+    )
 };
 
 export default IndexPage;
+
+type Imgae={
+    url:string;
+}
+
+const fetchImage=async():Promise<Imgae>=>{
+    const res=await fetch("https://api.thecatapi.com/v1/images/search");
+    const images =await res.json();
+    console.log(images);
+    return images[0];
+}
+
+fetchImage();
