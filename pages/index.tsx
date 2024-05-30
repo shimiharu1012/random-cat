e9a779573a907b66b59ea68106a840981f3b71b5
shimiharu1012 <shimizu1012@ezweb.ne.jp>
2024-05-30 19:13:18 +0900

    [修正]画像読み込み時の挙動の修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,56 @@ import {GetServerSideProps,NextPage} from "next";
 import { useEffect, useState } from "react";
 import styles from "../styles/index.module.css"
 
+
+// console.log(styles.loader<"string">.visibility)
 // GetServerSidePropsから渡される引数の型
 type Props={
     initialImageUrl: string;
 }
 
-const IndexPage: NextPage<Props>=({initialImageUrl})=>{
-    const [ImageUrl,setImageUrl]=useState(initialImageUrl);
-    const [loading,setLoading]=useState(false);
 
-    
+const IndexPage: NextPage<Props>=({initialImageUrl})=>{
+
+
+    // const loader=document.querySelector(styles.loader)
+    // console.log(loader)
+    // console.log(typeof(loader))
+
+    const [ImageUrl,setImageUrl]=useState(initialImageUrl);
+    // fecthing APIからの取得中はtrue,　取得後はfalse
+    // const [fecthing,setFetching]=useState(false);
+    // ページへの画像読み込み中はtrue，取得後はfalse
+    const [loading,setloading]=useState(false);
+
     const handleClick=()=>{
-        setLoading(true);
+        // setFetching(true);
+        setloading(true)
         fetchImage().then((newImage)=>{
             setImageUrl(newImage.url)
-            setLoading(false)
+            // setFetching(false)
         })
     }
 
+    const handleLoad=()=>{
+        setloading(false);
+    }
+
+    const color=loading ? styles.red : ""
+    const isLoaderVisible=loading ?  "" : styles.hidden;
+    const isImageVisible=loading ? styles.hidden : "";
+    // console.log(`isLoader ${isLoader}`)
+    // console.log(`isImage ${isImage}`)
+    console.log(color)
+
     return(
+        
         <div className={styles.container}>
             <button onClick={handleClick} className={styles.toNext}>他のニャンコも見る</button>
-            {console.log(ImageUrl)}
-            <div className={styles.imageContainer}>{loading ? <div className={styles.loader}></div>: <img className={styles.image} src={ImageUrl}/>}</div>
+            <div className={styles.imageContainer}>
+                {/* <div className={`${color}`}>hello</div> */}
+                <div className={`${styles.loader} ${isLoaderVisible}`}></div>
+                <img onLoad={handleLoad} className={`${styles.image} ${isImageVisible}`} src={ImageUrl}/>
+            </div>
         </div>
     )
 };
